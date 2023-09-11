8145a9bf853bdbf129666dfe57667118c4d4e3a2
Nada <nadanabil2307@gmail.com>
2023-09-11 11:02:49 +0300

    splash screen ui

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stylish/core/resources/theme_manager.dart';
+import 'package:stylish/features/splash/presentation/splash_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -7,8 +8,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Stylish App',
       theme: getAppTheme(),
+      home: SplashScreen(),
     );
   }
 }
