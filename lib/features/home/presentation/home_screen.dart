14969891ba1af054b232b5924664c944f983aaa2
Nada <nadanabil2307@gmail.com>
2023-09-18 10:04:07 +0300

    search widget

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stylish/core/widgets/search_widget.dart';
 
 import '../../../core/widgets/default_app_bar.dart';
 
@@ -7,8 +8,21 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var searchController = TextEditingController();
     return Scaffold(
       appBar: defaultAppBar(),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        padding: EdgeInsets.all(15),
+        child: Column(
+          children: [
+            SearchWidget(
+              onChange: (value) {},
+              controller: searchController,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
